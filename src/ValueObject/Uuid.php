160fc7e1a0c7e5ed6c4e647bce65689742074409
Shaun Freeman <shaun@shaunfreeman.co.uk>
2019-10-12 14:22:30 +0100

    added uuid value object

---
+++ src/ValueObject/Uuid.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace XDevApi\ValueObject;
+
+
+use Exception;
+use InvalidArgumentException;
+use JsonSerializable;
+
+class Uuid implements JsonSerializable
+{
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * Uuid constructor.
+     * @param string|null $uuid
+     * @throws Exception
+     */
+    public function __construct(?string $uuid = null)
+    {
+        if ($uuid && !$this->isValid($uuid)) {
+            throw new InvalidArgumentException(sprintf('Uuid "%s" is not a valid uuid.', $uuid));
+        }
+
+        $this->id = $uuid ? $this->parse($uuid) : $this->generate();
+    }
+
+    /**
+     * @param $uuid
+     * @return bool
+     */
+    public function isValid($uuid): bool
+    {
+        return preg_match(
+            '/^[a-f0-9]{8}-?[a-f0-9]{4}-?[a-f0-9]{4}-?[a-f0-9]{4}-?[a-f0-9]{12}$/',
+            $uuid
+        ) === 1;
+    }
+
+    /**
+     * @return string
+     */
+    public function toString(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->toString();
+    }
+
+    /**
+     * @return string
+     */
+    public function jsonSerialize():string
+    {
+        return $this->toString();
+    }
+
+    /**
+     * @return string
+     */
+    public function getHex(): string
+    {
+        return str_replace('-', '', $this->toString());
+    }
+
+    /**
+     * @param $hex
+     * @return string
+     */
+    private function parse($hex): string
+    {
+        preg_match(
+            '/^([a-f0-9]{8})-?([a-f0-9]{4})-?([a-f0-9]{4})-?([a-f0-9]{4})-?([a-f0-9]{12})$/',
+            $hex,
+            $matches
+        );
+
+        // remove first item in array match.
+        array_shift($matches);
+
+        return vsprintf('%08s-%04s-%04s-%04s-%012s', $matches);
+    }
+
+    /**
+     * Cobbled together from ramsey/uuid
+     * @return string
+     * @throws Exception
+     */
+    private function generate(): string
+    {
+        $hex        = bin2hex(random_bytes(16));
+        $timeHi     = hexdec(substr($hex, 12, 4)) & 0x0fff & ~(0xf000) | 4 << 12;
+        $clockSeqHi = hexdec(substr($hex, 16, 2)) & 0x3f & ~(0xc0) | 0x80;
+
+        $fields = [
+            'time_low'                  => substr($hex, 0, 8),
+            'time_mid'                  => substr($hex, 8, 4),
+            'time_hi_and_version'       => dechex($timeHi),
+            'clock_seq_hi_and_reserved' => dechex($clockSeqHi),
+            'clock_seq_low'             => substr($hex, 18, 2),
+            'node'                      => substr($hex, 20, 12),
+        ];
+
+        return vsprintf('%08s-%04s-%04s-%02s%02s-%012s', $fields);
+    }
+}
---
+++ test/ValueObject/UuidTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace XDevApiTest\ValueObject;
+
+use InvalidArgumentException;
+use JsonSerializable;
+use XDevApi\ValueObject\Uuid;
+use PHPUnit\Framework\TestCase;
+
+class UuidTest extends TestCase
+{
+    private $regex = '/^([a-f0-9]{8})-([a-f0-9]{4})-([a-f0-9]{4})-([a-f0-9]{4})-([a-f0-9]{12})$/';
+
+    public function testCanCreateUuid()
+    {
+        $uuid = new Uuid();
+        $this->assertInstanceOf(Uuid::class, $uuid);
+    }
+
+    public function testCanGenerateNewUuid()
+    {
+        $uuid = new Uuid();
+        $result = preg_match($this->regex, (string) $uuid);
+
+        $this->assertTrue((bool) $result);
+    }
+
+    public function testCanCastToString()
+    {
+        $uuid = new Uuid();
+        $this->assertIsString($uuid->toString());
+    }
+
+    public function testCanValidateUuid()
+    {
+        $uuid = new Uuid();
+        $this->assertTrue($uuid->isValid($uuid->toString()));
+
+    }
+
+    public function testCanParseExistingUuid()
+    {
+        $uuid = new Uuid('5fd47c55b2d348bb9888f9679f18f290');
+        $this->assertSame('5fd47c55-b2d3-48bb-9888-f9679f18f290', $uuid->toString());
+    }
+
+    public function testCanMalFormedUuidThrowsException()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Uuid('5fd4-7c55b2d34-8bb9888f-9679f18-f290');
+
+    }
+
+    public function testCanReturnJsonSerialise()
+    {
+        $uuid = new Uuid('d1ec9c0f-4bea-41a8-84eb-3d79d39e5025');
+        $this->assertInstanceOf(JsonSerializable::class, $uuid);
+        $this->assertSame('"d1ec9c0f-4bea-41a8-84eb-3d79d39e5025"', json_encode($uuid));
+    }
+
+    public function testGetHex()
+    {
+        $uuid = new Uuid('d1ec9c0f-4bea-41a8-84eb-3d79d39e5025');
+        $this->assertSame('d1ec9c0f4bea41a884eb3d79d39e5025', $uuid->getHex());
+    }
+}
+
